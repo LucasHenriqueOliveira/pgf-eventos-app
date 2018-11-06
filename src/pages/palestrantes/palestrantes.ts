01c36d468ccdc1bbc7835dcdd332cd85b94683a2
Lucas <lucashen@gmail.com>
2018-11-06 20:16:30 -0200

    create palestrantes component

---
+++ src/pages/palestrantes/palestrantes.ts
@@ -0,0 +1,69 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DataProvider } from '../../providers/data/data';
+import { PalestrantePage } from '../../pages/palestrante/palestrante';
+
+/**
+ * Generated class for the PalestrantesPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-palestrantes',
+  templateUrl: 'palestrantes.html',
+})
+export class PalestrantesPage {
+
+  searchQuery: string = '';
+  items: any;
+  arrItems: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private Data: DataProvider) {
+    this.initializeItems();
+  }
+
+  initializeItems() {
+    this.Data.getPalestrantes().subscribe(
+      result => {
+        this.items = result;
+        this.arrItems = this.items;
+      },
+      error => {
+        // this.loading = false;
+        // this.notify.error('Erro ao retornar o status', {timeout: 3000, showProgressBar: false });
+      }
+    );
+  }
+
+  ionViewDidLoad() {}
+
+  query = (items, filters) => {
+    return items.filter((item) => {
+      return filters.reduce((found, filter) => {
+        if (!(item[filter.key].includes(filter.value))) return false
+        return found
+      }, true)
+    });
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.items = this.arrItems;
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.query(this.items, [{key: 'nome', value: val}]);
+    }
+  }
+
+  getPalestrante(item) {
+    this.navCtrl.push(PalestrantePage, {id: item.id});
+  }
+
+}
