5e2756e3b00caa0b4547202bbfd936bccb879472
Lucas <lucashen@gmail.com>
2018-11-08 18:10:48 -0200

    change login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Storage } from '@ionic/storage';
+import { DataProvider } from '../../providers/data/data';
+import { ToastController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the LoginPage page.
@@ -15,11 +20,68 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private formLogin: FormGroup;
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  dataToken: any = [];
+	dataUser: any = [];
+  dataForgotEmail: any = [];
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder,
+    private storage: Storage, public loadingCtrl: LoadingController, private Data: DataProvider,
+    private toastCtrl: ToastController) {
+    this.formLogin = this.formBuilder.group({
+			email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+			password: this.formBuilder.control('', [Validators.required])
+			})
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
   }
 
+  signup() {
+		//this.navCtrl.push(CadastroPage);
+  }
+  
+  login(login) {
+		this.storage.clear();
+		let loader = this.loadingCtrl.create({content: "Aguarde..."});
+		loader.present();
+
+		this.Data.login(login).subscribe(data => {
+      this.dataToken = data;
+			this.storage.set('token', this.dataToken.access_token);
+
+			this.Data.getUser(this.dataToken.user.id).subscribe(res => {
+
+				this.dataUser = res;
+        this.storage.set('user', this.dataToken.user);
+        this.storage.set('programacao', res);
+				loader.dismiss();
+        this.navCtrl.push(HomePage);
+        
+      }, err => {
+
+				this.storage.clear();
+				loader.dismiss();
+				let toast = this.toastCtrl.create({
+					message: 'Usuário não encontrado!',
+					duration: 3000,
+					position: 'middle'
+        });
+        toast.present();
+        
+      })
+    }, err => {
+
+			this.storage.clear();
+      loader.dismiss();
+			let toast = this.toastCtrl.create({
+				message: 'Usuário não encontrado!',
+				duration: 3000,
+				position: 'middle'
+			});
+      toast.present();
+
+    })
+	}
 }
