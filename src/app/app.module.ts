163fb90aff93abb465ea8f83384af2ab7419ce0e
Lucas <lucashen@gmail.com>
2018-11-14 14:40:45 -0200

    change layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,76 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ProgramacaoPage } from '../pages/programacao/programacao';
+import { PalestrantesPage } from '../pages/palestrantes/palestrantes';
+import { InscricaoPage } from '../pages/inscricao/inscricao';
+import { VotacaoPage } from '../pages/votacao/votacao';
+import { LoginPage } from '../pages/login/login';
+import { DataProvider } from '../providers/data/data';
+import { HttpClientModule } from '@angular/common/http'; 
+import { PalestrantePage } from '../pages/palestrante/palestrante';
+import { AppVersion } from '@ionic-native/app-version';
+import { EventoPage } from '../pages/evento/evento';
+import { IntroPage } from '../pages/intro/intro';
+import { CadastroPage } from '../pages/cadastro/cadastro';
+import { PerguntaPage } from '../pages/pergunta/pergunta';
+import { VotarPage } from '../pages/votar/votar';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    LoginPage,
+    ProgramacaoPage,
+    PalestrantesPage,
+    PalestrantePage,
+    InscricaoPage,
+    VotacaoPage,
+    EventoPage,
+    IntroPage,
+    CadastroPage,
+    PerguntaPage,
+    VotarPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp, {
+      backButtonText: '',
+      backButtonIcon: 'ios-arrow-back',
+      iconMode: 'md'
+    }),
+    HttpClientModule,
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    LoginPage,
+    ProgramacaoPage,
+    PalestrantesPage,
+    PalestrantePage,
+    InscricaoPage,
+    VotacaoPage,
+    EventoPage,
+    IntroPage,
+    CadastroPage,
+    PerguntaPage,
+    VotarPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataProvider,
+    AppVersion
   ]
 })
 export class AppModule {}
