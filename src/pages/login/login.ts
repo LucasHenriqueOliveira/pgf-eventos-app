a280cef86ac2110fcd70d8051314087a707a598c
Lucas <lucashen@gmail.com>
2018-11-14 14:40:26 -0200

    add esqueceu a senha

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Storage } from '@ionic/storage';
 import { DataProvider } from '../../providers/data/data';
 import { ToastController } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { CadastroPage } from '../cadastro/cadastro';
 
 /**
  * Generated class for the LoginPage page.
@@ -28,18 +29,18 @@ export class LoginPage {
   
   constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder,
     private storage: Storage, public loadingCtrl: LoadingController, private Data: DataProvider,
-    private toastCtrl: ToastController) {
+    private toastCtrl: ToastController, public alertCtrl: AlertController) {
     this.formLogin = this.formBuilder.group({
 			email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
 			password: this.formBuilder.control('', [Validators.required])
-			})
+      });
   }
 
   ionViewDidLoad() {
   }
 
   signup() {
-		//this.navCtrl.push(CadastroPage);
+		this.navCtrl.push(CadastroPage);
   }
   
   login(login) {
@@ -76,12 +77,69 @@ export class LoginPage {
 			this.storage.clear();
       loader.dismiss();
 			let toast = this.toastCtrl.create({
-				message: 'Usuário não encontrado!',
+				message: err.error.error,
 				duration: 3000,
 				position: 'middle'
 			});
       toast.present();
 
     })
+  }
+  
+  esqueceuSenha() {
+		let prompt = this.alertCtrl.create({
+		  title: 'Esqueceu a senha?',
+		  message: "Entre com o seu e-mail cadastrado",
+		  inputs: [
+			{
+			  name: 'email',
+			  placeholder: 'Seu e-mail'
+			},
+		  ],
+		  buttons: [
+			{
+			  text: 'Cancelar'
+			},
+			{
+			  text: 'Enviar',
+			  handler: data => {
+				let loader = this.loadingCtrl.create({content: "Aguarde..."});
+				loader.present();
+
+				this.Data.esqueceuSenha(data).subscribe(res => {
+					this.dataForgotEmail = res
+					loader.dismiss();
+					if(this.dataForgotEmail.error) {
+						let toast = this.toastCtrl.create({
+							message: this.dataForgotEmail.message,
+							duration: 3000,
+							position: 'middle',
+							cssClass: 'toast-error'
+						});
+						toast.present();
+					} else {
+						let toast = this.toastCtrl.create({
+							message: "Enviamos um email com informações da nova senha!",
+							duration: 3000,
+							position: 'middle',
+							cssClass: 'toast-success'
+						});
+						toast.present();
+					}
+				  }, err => {
+					loader.dismiss();
+					let toast = this.toastCtrl.create({
+						message: 'Erro ao recuperar a senha do usuário!',
+						duration: 3000,
+						position: 'middle',
+						cssClass: 'toast-error'
+					});
+					toast.present();
+				  })
+			  }
+			}
+		  ]
+		});
+		prompt.present();
 	}
 }
